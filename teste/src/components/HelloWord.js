54ddc3452677ce2751ba58bb8f966f68110275a6
JESSICA PEREIRA DOS SANTOS <23111734@CFP127ALUNO.LOCAL>
2024-08-15 12:22:14 -0300

    adicionar

---
+++ teste/src/components/HelloWord.js
@@ -0,0 +1,12 @@
+import Frase from './Frase'
+function HelloWord(){
+
+    return (
+        <div>
+            <Frase/>
+            <h1>MEU PRIMEIRO COMPONENTE</h1>
+        </div>
+    )
+}
+
+export default HelloWord
